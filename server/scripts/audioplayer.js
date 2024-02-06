a00c23d11947ffad91d4f5d0b49236c6391a36c7
RyanTheTechMan <ryan@rivergroup.com>
2024-02-06 12:36:52 -0500

    Songs will now replay once end of playlist
    Also started work on volume slider.

--- server/scripts/audioplayer.js
+++ server/scripts/audioplayer.js
@@ -1,31 +1,54 @@
 const audioPlayer = document.querySelector('#audioPlayer');
 
+let wasAutoplayBlocked = false;
+
 fetch('/audio-files')
 	.then((response) => response.json())
 	.then((files) => {
 		let currentFileIndex = Math.floor(Math.random() * files.length);
 
 		function playNextFile() {
-			if (currentFileIndex < files.length) {
-				audioPlayer.src = `/audio/${files[currentFileIndex]}`;
-				audioPlayer.play();
-				currentFileIndex += 1;
+			if (currentFileIndex >= files.length) {
+				currentFileIndex = 0;
 			}
+
+			audioPlayer.src = `/audio/${files[currentFileIndex]}`;
+			audioPlayer.play().catch(() => {
+				audioPlayer.muted = true;
+				wasAutoplayBlocked = true;
+			});
+
+			currentFileIndex += 1;
 		}
 
 		audioPlayer.onended = playNextFile;
+
 		playNextFile();
 	})
 	.catch((error) => console.error('Error:', error));
 
 const volumeToggle = document.querySelector('#ToggleVolume');
 
+audioPlayer.addEventListener('volumechange', () => {
+	volumeToggle.src = audioPlayer.muted ? 'images/nav/ic_volume_off_white_24dp_2x.png' : 'images/nav/ic_volume_up_white_24dp_2x.png';
+});
+
+// const volumeSlider = document.querySelector('#VolumeSlider');
+
 volumeToggle.addEventListener('click', () => {
 	if (audioPlayer.muted) {
 		audioPlayer.muted = false;
-		volumeToggle.src = 'images/nav/ic_volume_up_white_24dp_2x.png';
+		// volumeSlider.style.display = 'block';
+		if (wasAutoplayBlocked) {
+			audioPlayer.play();
+			wasAutoplayBlocked = false;
+		}
 	} else {
 		audioPlayer.muted = true;
-		volumeToggle.src = 'images/nav/ic_volume_off_white_24dp_2x.png';
+		// volumeSlider.style.display = 'none';
 	}
 });
+
+// volumeSlider.addEventListener('input', () => {
+// 	audioPlayer.volume = volumeSlider.value;
+// });
